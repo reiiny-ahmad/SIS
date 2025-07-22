6881ee753a11310b1a779c98e2311d3dc71f9c90
jabri ahmed <124925179+reiiny-ahmad@users.noreply.github.com>
2025-07-22 23:26:32 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import pyrebase
+import firebase_admin
+from firebase_admin import credentials, firestore
 
+# Configuration Pyrebase
 firebase_config = {
     "apiKey": "AIzaSyBkt3AmdkaKfiM5SUFrKeQLUwosJq-Xupo",
     "authDomain": "steps-into-space.firebaseapp.com",
@@ -10,5 +13,11 @@ firebase_config = {
     "appId": "1:554363529046:web:a3f6e550c44746d20498f7"
 }
 
+# Initialisation Pyrebase
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
+
+# Initialisation Admin SDK (pour Firestore uniquement)
+cred = credentials.Certificate("steps-into-space-firebase-adminsdk.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()  # Ceci rend 'db' disponible pour l'importation
