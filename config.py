3b413496c8b4e295771767b5a5bca5b83708c4af
jabri ahmed <124925179+reiiny-ahmad@users.noreply.github.com>
2025-07-22 23:13:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,8 @@
 import firebase_admin
-from firebase_admin import credentials, firestore, auth as firebase_auth
-import pyrebase
+from firebase_admin import credentials, firestore
 
-
-# Configuration pour Pyrebase (authentification uniquement)
-firebase_config = {
-    "apiKey": "AIzaSyBkt3AmdkaKfiM5SUFrKeQLUwosJq-Xupo",
-    "authDomain": "steps-into-space.firebaseapp.com",
-    "databaseURL": "https://steps-into-space.firebaseio.com",  # <-- Ajoute cette ligne
-    "projectId": "steps-into-space",
-    "storageBucket": "steps-into-space.appspot.com",  # <-- Corrige ici (ajoute .com)
-    "messagingSenderId": "554363529046",
-    "appId": "1:554363529046:web:a3f6e550c44746d20498f7"
-}
-
-# Initialiser Pyrebase pour l'authentification
-firebase = pyrebase.initialize_app(firebase_config)
-auth = firebase.auth()
-
-# Initialiser Firebase Admin pour Firestore
+# Configuration Firebase Admin uniquement
 cred = credentials.Certificate("steps-into-space-firebase-adminsdk.json")
-firebase_admin.initialize_app(cred, {
-    'projectId': 'steps-into-space',
-})
+firebase_admin.initialize_app(cred)
+
 db = firestore.client()
