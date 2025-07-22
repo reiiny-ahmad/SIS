4da378bfa3d77eb2a7cfd6c323e3d250f5b9fbc8
jabri ahmed <124925179+reiiny-ahmad@users.noreply.github.com>
2025-07-22 23:22:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+import pyrebase
 
-# Configuration Firebase Admin uniquement
-cred = credentials.Certificate("steps-into-space-firebase-adminsdk.json")
-firebase_admin.initialize_app(cred)
+firebase_config = {
+    "apiKey": "AIzaSyBkt3AmdkaKfiM5SUFrKeQLUwosJq-Xupo",
+    "authDomain": "steps-into-space.firebaseapp.com",
+    "databaseURL": "https://steps-into-space.firebaseio.com",
+    "projectId": "steps-into-space",
+    "storageBucket": "steps-into-space.appspot.com",
+    "messagingSenderId": "554363529046",
+    "appId": "1:554363529046:web:a3f6e550c44746d20498f7"
+}
 
-db = firestore.client()
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
